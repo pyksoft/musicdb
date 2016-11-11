5cdc8995c4688a2c2f1d2a51b21e420f71c61d7b
pyksoft <pyksoft@gmail.com>
2016-11-11 16:06:09 +0100

    sort by name

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -1,12 +1,22 @@
 class ArtistsController < ApplicationController
+  helper_method :sort_column, :sort_direction
 
   def index
     @artists = Artist.all
+    @artists = Artist.order(sort_column + ' ' + sort_direction)
   end
 
   def show
     @artist = Artist.find(params[:id])
     @photos = @artist.artistphotos
     @genres = @artist.genres
+  end
+
+  def sort_column
+    Artist.column_names.include?(params[:sort]) ? params[:sort] : "name"
+  end
+
+  def sort_direction
+    %w[asc desc].include?(params[:direction]) ? params[:direction] : "asc"
   end
 end
