9086c626e49474740bb34c3377ea44b3e6a642b5
pyksoft <pyksoft@gmail.com>
2016-11-12 10:02:41 +0100

    searcbar

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -2,8 +2,13 @@ class ArtistsController < ApplicationController
   helper_method :sort_column, :sort_direction
 
   def index
-    @artists = Artist.all
-    @artists = Artist.order(sort_column + ' ' + sort_direction)
+    if params[:search]
+      @artists = Artist.search(params[:search]) unless params[:search].blank?
+    elsif sort_direction
+      @artists = Artist.order(sort_column + ' ' + sort_direction)
+    else
+      @artists = Artist.all
+    end
   end
 
   def show
--- app/models/artist.rb
+++ app/models/artist.rb
@@ -2,9 +2,12 @@ class Artist < ApplicationRecord
   has_many :artistphotos
   has_and_belongs_to_many :genres
 
-def self.search(search)
-    where('name ILIKE ?', "%#{search}%")
-  else
-    scoped
+  def self.search(search)
+    if search
+    where('name ILIKE :search', search: "%#{search}%")
+    else
+    order(sort_column + ' ' + sort_direction)
+    end
   end
+
 end
